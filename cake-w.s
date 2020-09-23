05537b08ba52ad3a5e4f512631d64aea34904b08
sg4e <46875746+sg4e@users.noreply.github.com>
2020-09-22 23:55:30 -0400

    Add license

--- cake-w.s
+++ cake-w.s
@@ -1,3 +1,17 @@
+; Copyright (C) 2020 sg4e
+;
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, version 3.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;----- Aliases/Labels ----------------------------------------------------------
 ; these are aliases for the Memory Mapped Registers we will use
 INIDISP     = $2100     ; inital settings for screen
